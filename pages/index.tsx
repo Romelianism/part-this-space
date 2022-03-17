a7bc4d77a915c0c4478ef6a0969eba94dbca6a94
Romelianism <Romelianism@gmail.com>
2022-03-17 14:01:00 +0800

    feat: add space

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
+  const spaces_name = new Array<string>(10);
+  for (let i = 0; i < spaces_name.length; i++) {
+    spaces_name[i] = `Space${i}`;
+  }
+
   return (
     <div>
       <Head>
@@ -12,51 +18,23 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p>
-          Get started by editing <code>pages/index.tsx</code>
-        </p>
-
         <div>
-          <a href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {spaces_name.map((value) => (
+            <li key={value}>
+              <Link href={`/space/${value}`}>
+                <a>
+                  <Image
+                    src={`/images/space/${value}`}
+                    alt={value}
+                    width={100}
+                    height={100}
+                  />
+                </a>
+              </Link>
+            </li>
+          ))}
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
