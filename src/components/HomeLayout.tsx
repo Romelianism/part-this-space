f6dacddf0fd9e11b401ce9b9d29fba0819f6b11c
Romelianism <Romelianism@gmail.com>
2022-03-20 01:25:11 +0800

    fix: add valid icon size to stop warning
    i had this warning in the console "Unexpected value 1.5rem parsing
    height attribute.", so i converted rem to px size to be valid

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -11,41 +11,44 @@ import {
 } from "react-icons/md";
 import { SimpleHeader } from "./SimpleHeader";
 
-const HomeLayout = ({ children }: { children: React.ReactNode }) => (
-  <AppShell
-    header={
-      <SimpleHeader
-        links={[
-          {
-            link: "/",
-            label: "Home",
-            activeIcon: <MdHome size={"1.5rem"} />,
-            inactiveIcon: <MdOutlineHome size={"1.5rem"} />,
-          },
-          {
-            link: "/Contact",
-            label: "Contact",
-            activeIcon: <MdContacts size={"1.5rem"} />,
-            inactiveIcon: <MdOutlineContacts size={"1.5rem"} />,
-          },
-          {
-            link: "/Favorites",
-            label: "Favorites",
-            activeIcon: <MdFavorite size={"1.5rem"} />,
-            inactiveIcon: <MdFavoriteBorder size={"1.5rem"} />,
-          },
-          {
-            link: "/Settings",
-            label: "Settings",
-            activeIcon: <MdSettings size={"1.5rem"} />,
-            inactiveIcon: <MdOutlineSettings size={"1.5rem"} />,
-          },
-        ]}
-      />
-    }
-  >
-    {children}
-  </AppShell>
-);
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  const iconSize = 24;
+  return (
+    <AppShell
+      header={
+        <SimpleHeader
+          links={[
+            {
+              link: "/",
+              label: "Home",
+              activeIcon: <MdHome size={iconSize} />,
+              inactiveIcon: <MdOutlineHome size={iconSize} />,
+            },
+            {
+              link: "/Contact",
+              label: "Contact",
+              activeIcon: <MdContacts size={iconSize} />,
+              inactiveIcon: <MdOutlineContacts size={iconSize} />,
+            },
+            {
+              link: "/Favorites",
+              label: "Favorites",
+              activeIcon: <MdFavorite size={iconSize} />,
+              inactiveIcon: <MdFavoriteBorder size={iconSize} />,
+            },
+            {
+              link: "/Settings",
+              label: "Settings",
+              activeIcon: <MdSettings size={iconSize} />,
+              inactiveIcon: <MdOutlineSettings size={iconSize} />,
+            },
+          ]}
+        />
+      }
+    >
+      {children}
+    </AppShell>
+  );
+};
 
 export default HomeLayout;
